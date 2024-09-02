ab083ca9f5ae81a05509dd911ea6ea8cb28983fe
gnavien <gnavien@gmail.com>
2024-09-02 08:11:51 +0530

    update

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@
 #  source = "git::https://github.com/gnavien/tf-module-app.git"
 #  env = var.env
 #}
+
+
+module "instances" {
+  for_each = var.components
+  source = "git::https://github.com/gnavien/tf-module-app.git"
+  component = each.key
+  env = var.env
+}
