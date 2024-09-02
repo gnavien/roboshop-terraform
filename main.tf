7d7306692f3707b28bd7ad7a391a7379be832682
gnavien <gnavien@gmail.com>
2024-09-02 10:26:54 +0530

    update

--- main.tf
+++ main.tf
@@ -2,9 +2,6 @@
 #  source = "git::https://github.com/gnavien/tf-module-app.git"
 #  env = var.env
 #}
-provider "aws" {
-  region = "us-east-1"
-}
 
 module "instances" {
   source = "git::https://github.com/gnavien/tf-module-app.git"
--- state.tf
+++ state.tf
@@ -1,3 +1,7 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "prod/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
