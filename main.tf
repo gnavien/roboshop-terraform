c5d166cd1309c151f41221eae8909d7adae87085
gnavien <gnavien@gmail.com>
2024-10-02 11:10:52 +0530

    update

--- main.tf
+++ main.tf
@@ -11,13 +11,24 @@
 #}
 
 module "vpc" {
-  source = "git::https://github.com/gnavien/tf-module-vpc.git"
-  for_each = var.vpc
+  source     = "git::https://github.com/gnavien/tf-module-vpc.git"
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
-  subnets = each.value["subnets"] # this value we will get from main.tfvars
+  subnets    = each.value["subnets"] # this value we will get from main.tfvars
 
   env = var.env
   tags = var.tags
+  default_vpc_id = var.default_vpc_id
 
 
+}
+
+
+module "app_server" {
+  source = "git::https://github.com/gnavien/tf-module-app.git"
+  component = "test"
+  env = var.env
+  tags = var.tags
+  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "app", null), "subnet_ids", null)[0]
+  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
 variable "env" {}
 variable "vpc" {} # we have mentioned in main.tfvars
 variable "tags" {}
+variable "default_vpc_id" {}
+variable "subnet_id" {}
+
