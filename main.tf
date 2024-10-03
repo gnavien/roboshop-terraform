1e320d55f2567db544ef47858b8e9ff3646c8aee
gnavien <gnavien@gmail.com>
2024-10-03 10:28:34 +0530

    update

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@
 
 module "vpc" {
   source     = "git::https://github.com/gnavien/tf-module-vpc.git"
-  for_each   = var.vpc_id
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
   subnets    = each.value["subnets"] # this value we will get from main.tfvars
 
