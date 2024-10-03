49be76021446aafe65ce342fac45a8d0fb3b0f23
gnavien <gnavien@gmail.com>
2024-10-03 08:43:09 +0530

    update

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "env" {}
 variable "vpc" {} # we have mentioned in main.tfvars
 variable "tags" {}
-variable "default_vpc_id" {}
-variable "subnet_id" {}
+#variable "default_vpc_id" {}
+#variable "subnet_id" {}
 
