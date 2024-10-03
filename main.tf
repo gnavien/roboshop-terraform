8d02c4f15c84090148ac407b391605ec95fd5405
gnavien <gnavien@gmail.com>
2024-10-03 08:23:26 +0530

    update

--- main.tf
+++ main.tf
@@ -23,12 +23,12 @@ module "vpc" {
 
 }
 
-
-module "app_server" {
-  source = "git::https://github.com/gnavien/tf-module-app.git"
-  component = "test"
-  env = var.env
-  tags = var.tags
-  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "app", null), "subnet_ids", null)[0]
-  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-}
+#
+#module "app_server" {
+#  source = "git::https://github.com/gnavien/tf-module-app.git"
+#  component = "test"
+#  env = var.env
+#  tags = var.tags
+#  subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "app", null), "subnet_ids", null)[0]
+#  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#}
