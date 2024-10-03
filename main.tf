b548d330672591e98e9dbf650b794db77c8aceee
gnavien <gnavien@gmail.com>
2024-10-03 09:49:38 +0530

    update

--- main.tf
+++ main.tf
@@ -39,6 +39,7 @@ module "vpc" {
   source     = "git::https://github.com/gnavien/tf-module-vpc.git"
   for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
+  web_subnet_cidr_block = each.value["web_subnet_cidr_block"]
   env = var.env
   tags = var.tags
 }
