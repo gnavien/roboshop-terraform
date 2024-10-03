827995798a3f03f17993a9d38c101a02d7bf9657
gnavien <gnavien@gmail.com>
2024-10-03 11:33:29 +0530

    update

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
   subnets    = each.value["subnets"]
-  vpc_id = each.value["vpc_id"]
+  vpc_id = var.vpc_id
 
   env            = var.env
   tags           = var.tags
