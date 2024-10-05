adb598c7d271bbb6c263aab9803d7fc20f920840
gnavien <gnavien@gmail.com>
2024-10-05 08:06:11 +0530

    update

--- variables.tf
+++ variables.tf
@@ -3,10 +3,9 @@ variable "tags" {}
 variable "vpc" {}
 variable "default_vpc_id" {}
 variable "default_rt_table" {}
-variable "instance_type" {}
-variable "component" {}
-variable "sg_subnet_cidr" {}
-variable "vpc_id" {}
+#variable "instance_type" {}
+#variable "sg_subnet_cidr" {}
+#variable "vpc_id" {}
 variable "rabbitmq" {}
 variable "allow_ssh_cidr" {}
 
