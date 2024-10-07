89ae0761a8258fd1cee9b802d4152a7e94992bda
gnavien <gnavien@gmail.com>
2024-10-07 22:04:39 +0530

    update

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 module "vpc" {
   source = "git::https://github.com/gnavien/tf-module-vpc.git"
 
-  for_each   = var.vpc
-  cidr_block = each.value["cidr_block"]
-  subnets    = each.value["subnets"]
-  env            = var.env
-  tags           = var.tags
-  default_vpc_id = var.default_vpc_id
+  for_each         = var.vpc
+  cidr_block       = each.value["cidr_block"]
+  subnets          = each.value["subnets"]
+  env              = var.env
+  tags             = var.tags
+  default_vpc_id   = var.default_vpc_id
   default_rt_table = var.default_rt_table
 }
 
 module "app_server" {
-  source = "git::https://github.com/gnavien/tf-module-app.git"
-  env = var.env
-  tags = var.tags
+  source    = "git::https://github.com/gnavien/tf-module-app.git"
+  env       = var.env
+  tags      = var.tags
   component = "test"
   subnet_id = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null ), "app", null), "subnet_ids", null)[0]
-  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  vpc_id    = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
 
 }
 
@@ -36,31 +36,34 @@ module "rabbitmq" {
 
   # Below are the plain variables which does not belong to iterations
 
-  env          = var.env
-  tags         = var.tags
-  allow_ssh_cidr  = var.allow_ssh_cidr
-  zone_id = var.zone_id
-
-
-}
-
-
-module "rds" {
-  source = "git::https://github.com/gnavien/tf-module-rds.git"
-
-  for_each       = var.rds
-  component      = each.value["component"]
-  engine         = each.value["engine"]
-  engine_version = each.value["engine_version"]
-  db_name        = each.value["db_name"]
-  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-  instance_count = each.value["instance_count"]
-  instance_class = each.value["instance_class"]
-  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-
-  tags           = var.tags
   env            = var.env
-  #kms_key_arn    = var.kms_key_arn
-  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  tags           = var.tags
+  allow_ssh_cidr = var.allow_ssh_cidr
+  zone_id        = var.zone_id
+  #  kms_key_arn    = var.kms_key_arn
+  kms_key_id     = var.kms_key_id
+
+
 
 }
+
+
+#module "rds" {
+#  source = "git::https://github.com/gnavien/tf-module-rds.git"
+#
+#  for_each       = var.rds
+#  component      = each.value["component"]
+#  engine         = each.value["engine"]
+#  engine_version = each.value["engine_version"]
+#  db_name        = each.value["db_name"]
+#  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+#  instance_count = each.value["instance_count"]
+#  instance_class = each.value["instance_class"]
+#  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#
+#  tags           = var.tags
+#  env            = var.env
+#  #kms_key_arn    = var.kms_key_arn
+#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+#
+#}
