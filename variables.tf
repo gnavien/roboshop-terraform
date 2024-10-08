ff4135d67539c20e92894020c495822d977d84ad
gnavien <gnavien@gmail.com>
2024-10-08 07:14:55 +0530

    update

--- variables.tf
+++ variables.tf
@@ -3,9 +3,8 @@ variable "tags" {}
 variable "vpc" {}
 variable "default_vpc_id" {}
 variable "default_rt_table" {}
-#variable "instance_type" {}
-#variable "sg_subnet_cidr" {}
-#variable "vpc_id" {}
+variable "sg_subnet_cidr" {}
+variable "vpc_id" {}
 variable "rabbitmq" {}
 variable "allow_ssh_cidr" {}
 variable "zone_id" {}
