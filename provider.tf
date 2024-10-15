5888a1a6715e60663073970e1335d78b094207e7
gnavien <gnavien@gmail.com>
2024-10-15 21:25:44 +0530

    update

--- provider.tf
+++ provider.tf
@@ -1,12 +1,10 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source = "hashicorp/aws"
+      version = "5.70.0"
     }
   }
-
-  required_version = ">= 1.0.0"
 }
 
 provider "aws" {
