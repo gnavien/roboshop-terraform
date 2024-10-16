99782b4a8e3c12d5813a2bf951b998a0b0c17efe
gnavien <gnavien@gmail.com>
2024-10-16 16:45:13 +0530

    update

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "vpc" {
   default_rt_table = var.default_rt_table
 }
 
-#
+
 #module "rabbitmq" {
 #  source = "git::https://github.com/gnavien/tf-module-rabbitmq.git"
 #  # Below are the input variables
@@ -34,27 +34,27 @@ module "vpc" {
 #  kms_key_arn    = var.kms_key_arn
 #
 #}
-#
-#
-#module "rds" {
-#  source = "git::https://github.com/gnavien/tf-module-rds.git"
-#
-#  for_each       = var.rds
-#  component      = each.value["component"]
-#  engine         = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#  db_name        = each.value["db_name"]
-#  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-#  instance_count = each.value["instance_count"]
-#  instance_class = each.value["instance_class"]
-#  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-#
-#  tags           = var.tags
-#  env            = var.env
-#  kms_key_arn    = var.kms_key_arn  # kms key ARN (amazon resource name)
-#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
-#
-#}
+
+
+module "rds" {
+  source = "git::https://github.com/gnavien/tf-module-rds.git"
+
+  for_each       = var.rds
+  component      = each.value["component"]
+  engine         = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  db_name        = each.value["db_name"]
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+
+  tags           = var.tags
+  env            = var.env
+  kms_key_arn    = var.kms_key_arn  # kms key ARN (amazon resource name)
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+
+}
 #
 ## For Documentdb we are using instance based cluster, we are not creating using elastic cluster
 #module "documentdb" {
@@ -84,29 +84,29 @@ module "vpc" {
 #}
 #
 ##
-#module "elasticache" {
-#  source = "git::https://github.com/gnavien/tf-module-elasticache.git"
-#
-#  for_each       = var.elasticache
-#  component      = each.value["component"]
-#  engine         = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#  node_type = each.value["node_type"]
-#
-#  replicas_per_node_group = each.value["replicas_per_node_group"]
-#  num_node_groups = each.value["num_node_groups"]
-#  parameter_group_name = each.value["parameter_group_name"]
-#
-#  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-#  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
-#  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-#
-#
-#  tags           = var.tags
-#  env            = var.env
-#  kms_key_arn    = var.kms_key_arn  # kms key ARN (amazon resource name)
-#
-#}
+module "elasticache" {
+  source = "git::https://github.com/gnavien/tf-module-elasticache.git"
+
+  for_each       = var.elasticache
+  component      = each.value["component"]
+  engine         = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  node_type = each.value["node_type"]
+
+  replicas_per_node_group = each.value["replicas_per_node_group"]
+  num_node_groups = each.value["num_node_groups"]
+  parameter_group_name = each.value["parameter_group_name"]
+
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+
+
+  tags           = var.tags
+  env            = var.env
+  kms_key_arn    = var.kms_key_arn  # kms key ARN (amazon resource name)
+
+}
 
 module "alb" {
   source             = "git::https://github.com/gnavien/tf-module-alb.git"
