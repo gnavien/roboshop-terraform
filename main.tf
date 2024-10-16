94ff47142d14ceb6b6104200ae8a460220b8c437
gnavien <gnavien@gmail.com>
2024-10-16 15:53:00 +0530

    update

--- main.tf
+++ main.tf
@@ -107,53 +107,53 @@ module "vpc" {
 #  kms_key_arn    = var.kms_key_arn  # kms key ARN (amazon resource name)
 #
 #}
-
-module "alb" {
-  source             = "git::https://github.com/gnavien/tf-module-alb.git"
-  # Below are the input variables
-  for_each           = var.alb
-  name               = each.value["name"]
-  internal           = each.value["internal"]
-  load_balancer_type = each.value["load_balancer_type"]
-
-  vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  sg_subnet_cidr     = each.value["name"]  == "public" ? ["0.0.0.0/0"  ] : local.app_web_subnet_cidr       # Here we are giving 2 subnets app and web to access each other as the request will be coming from both the directiuons
-
-  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value ["subnet_ref"], null), "subnet_ids", null)
-  env  = var.env
-  tags = var.tags
-
-}
-
-module "apps" {
-
-    source = "git::https://github.com/gnavien/tf-module-app.git"
-
-    for_each           = var.apps
-    app_port           = each.value["app_port"]
-    desired_capacity   = each.value["desired_capacity"]
-    instance_type      = each.value["instance_type"]
-    max_size           = each.value["max_size"]
-    min_size           = each.value["min_size"]
-    component          = each.value["component"]
-    sg_subnet_cidr    = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
-
-    subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
-    vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-    lb_dns_name        = lookup(lookup(module.alb, each.value["lb_ref"], null), "dns_name", null)
-    listener_arn       = lookup(lookup(module.alb, each.value["lb_ref"], null), "listener_arn", null)
-    lb_rule_priority   = each.value["lb_rule_priority"]
-#    extra_param_access = try(each.value["extra_param_access"], [])
-
-    env                   = var.env
-    tags                  = var.tags
-    kms_key_id            = var.kms_key_arn
-    allow_ssh_cidr        = var.allow_ssh_cidr
-#    kms_arn               = var.kms_key_arn
-#    allow_prometheus_cidr = var.allow_prometheus_cidr
-
-
-}
+#
+#module "alb" {
+#  source             = "git::https://github.com/gnavien/tf-module-alb.git"
+#  # Below are the input variables
+#  for_each           = var.alb
+#  name               = each.value["name"]
+#  internal           = each.value["internal"]
+#  load_balancer_type = each.value["load_balancer_type"]
+#
+#  vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#  sg_subnet_cidr     = each.value["name"]  == "public" ? ["0.0.0.0/0"  ] : local.app_web_subnet_cidr       # Here we are giving 2 subnets app and web to access each other as the request will be coming from both the directiuons
+#
+#  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value ["subnet_ref"], null), "subnet_ids", null)
+#  env  = var.env
+#  tags = var.tags
+#
+#}
+#
+#module "apps" {
+#
+#    source = "git::https://github.com/gnavien/tf-module-app.git"
+#
+#    for_each           = var.apps
+#    app_port           = each.value["app_port"]
+#    desired_capacity   = each.value["desired_capacity"]
+#    instance_type      = each.value["instance_type"]
+#    max_size           = each.value["max_size"]
+#    min_size           = each.value["min_size"]
+#    component          = each.value["component"]
+#    sg_subnet_cidr    = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+#
+#    subnets            = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), each.value["subnet_ref"], null), "subnet_ids", null)
+#    vpc_id             = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#    lb_dns_name        = lookup(lookup(module.alb, each.value["lb_ref"], null), "dns_name", null)
+#    listener_arn       = lookup(lookup(module.alb, each.value["lb_ref"], null), "listener_arn", null)
+#    lb_rule_priority   = each.value["lb_rule_priority"]
+##    extra_param_access = try(each.value["extra_param_access"], [])
+#
+#    env                   = var.env
+#    tags                  = var.tags
+#    kms_key_id            = var.kms_key_arn
+#    allow_ssh_cidr        = var.allow_ssh_cidr
+##    kms_arn               = var.kms_key_arn
+##    allow_prometheus_cidr = var.allow_prometheus_cidr
+#
+#
+#}
 
 
 
